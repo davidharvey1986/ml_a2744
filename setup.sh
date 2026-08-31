7b56e1100abd41ed3dabb97f9e068eeb67d2ce4d
davidharvey1986@googlemail.com <davidharvey1986@googlemail.com>
2026-08-31 09:09:19 +0200

    updates

--- setup.sh
+++ setup.sh
@@ -17,6 +17,10 @@ source $ENV_NAME/bin/activate
 
 pip install --upgrade pip setuptools==59.8.0 wheel
 
+#SETUP tools with pkg
+
+python -m pip install "setuptools<81"
+
 # Core scientific stack
 
 pip install numpy==1.26.1 scipy pandas  matplotlib seaborn scikit-learn ipython ipykernel jupyter
